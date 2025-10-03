c6b554e8f8b68eb40b089fcb249f30af8371ae4c
Aoyub <ayoubelmoumen007@gmail.com>
2025-10-03 14:13:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+function somme()
+{
+
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a) + Number(b);
+    document.getElementById("t3").value = c;
+
+}
+
+function diff()
+{
+
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a) - Number(b);
+    document.getElementById("t3").value = c;
+
+}
+
+function produit()
+{
+
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a) * Number(b);
+    document.getElementById("t3").value = c;
+
+}
+
+function reste()
+{
+
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a) / Number(b);
+    document.getElementById("t3").value = c;
+
+}
+
+function parite()
+{
+    if (document.getElementById("t3").value %2== 0) {
+    document.getElementById("t4").value = "Pair";}
+
+    else{
+    document.getElementById("t4").value = "Impair";}
+
+}
+
+
+function permute()
+{
+    [document.getElementById("t1").value, document.getElementById("t2").value] = [document.getElementById("t2").value, document.getElementById("t1").value];   
+}
+
+
+
+function del()
+{
+    document.getElementById("t1").value = ''
+    document.getElementById("t2").value = ''
+    document.getElementById("t3").value = ''
+    document.getElementById("t4").value = ''
+
+}
